1be0f9d307bb3c51d1567335212917d2c73e1824
Lairlynx <vladut.lazar@student.unitbv.ro>
2026-07-04 13:58:44 +0300

    first comit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace W3LessonGround;
+
+/*
+ * The purpose of this project is to show proof of the completion of the W3Schools C# Tutorial
+ * It servers as proof of the lessons being completed, not the exercises, tests or exam. Those are proven by the certificate
+ * This project will be uploaded to GitHub under the account lairLynx - account owner Vladut-Constantin Lazar
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
